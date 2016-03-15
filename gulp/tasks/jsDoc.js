3357d51eabe07777b27645d728150e7aa4c85034
franmartosr <franciscomr1990@gmail.com>
2016-03-16 00:04:32 +0100

    [MODIFY] Moved jsDoc gulp.task to external file

---
+++ gulp/tasks/jsDoc.js
@@ -0,0 +1,15 @@
+﻿'use strict'
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var jsdoc = require('gulp-jsdoc-to-markdown');
+var markdownToHTML = require('gulp-markdown');
+var utils = require('../utils');
+
+module.exports = function() {
+  return gulp.src(utils.src.js)
+    .pipe(concat('jsdoc.md'))
+    .pipe(jsdoc())
+    .pipe(markdownToHTML())
+    .pipe(gulp.dest(utils.dest.docs));
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,11 @@
 ﻿'use strict'
 
-//Dependencias
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var todo = require('gulp-todo');
-var jsdoc = require('gulp-jsdoc-to-markdown');
-var markdownToHTML = require('gulp-markdown');
-
-var utils = require('./gulp/utils');
 
 gulp.task('esLint', require('./gulp/linterns/esLint'));
 gulp.task('minifyJS', require('./gulp/tasks/minifyJS'));
 gulp.task('todo', require('./gulp/tasks/todo'));
-//Tarea 'jsDoc'. Crea una documentacion HTML de todas las funciones.
-gulp.task('jsDoc', function() {
-  return gulp.src(utils.src.js)
-    .pipe(concat('jsdoc.md'))
-    .pipe(jsdoc())
-    .pipe(markdownToHTML())
-    .pipe(gulp.dest(utils.dest.docs));
-});
+gulp.task('jsDoc', require('./gulp/tasks/jsDoc'));
 gulp.task('sass', require('./gulp/tasks/sass'));
 
 /*********************WATCHERS*********************/
