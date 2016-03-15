2760a713ae6ce311e8cd58db68e2289575d9c564
franmartosr <franciscomr1990@gmail.com>
2016-03-15 23:23:45 +0100

    [MODIFY] Gulp.watch task moved to external file.

---
+++ gulp/watchers.js
@@ -0,0 +1,17 @@
+﻿'use strict'
+
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var jsToWatch = ['src/*.js', '!src/**/*.min.js'];
+var watchProps = {
+  interval: 1000,
+  debounceDelay: 2000
+};
+
+//Vigilante, al hacerlo como una tarea, no ejecuta nada por defecto al abrir.
+module.exports = function() {
+  gulp.watch(jsToWatch, watchProps, function(file) {
+    return runSequence('lint', 'minificarJS', 'jsDoc', 'todo');
+  });
+  gulp.watch('./css/scss/**/*.scss', watchProps, ['styles']);
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 ﻿'use strict'
 
 //Dependencias
-var gulp = require('gulp'),
-    eslint = require('gulp-eslint'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    todo = require('gulp-todo'),
-    jsdoc = require('gulp-jsdoc-to-markdown'),
-    markdownToHTML = require('gulp-markdown');
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var todo = require('gulp-todo');
+var jsdoc = require('gulp-jsdoc-to-markdown');
+var markdownToHTML = require('gulp-markdown');
+var runSequence = require('run-sequence');
 
 //Variables auxiliares
 var jsToWatch = ['src/*.js', '!src/**/*.min.js'];
@@ -22,21 +23,21 @@ gulp.task('lint', function() {
 });
 /*Tarea 'minificarJS'. Concat evita la compresión
   de la fuente y/o el problema del bucle infinito.*/
-gulp.task('minificarJS', ['lint'], function () {
+gulp.task('minificarJS', function () {
   return gulp.src(jsToWatch)
     .pipe(concat('all.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('src'));
 });
 //Tarea 'todo'. Lista los todo en un archivo HTML.
-gulp.task('todo', ['minificarJS'], function() {
+gulp.task('todo', function() {
   return gulp.src(jsToWatch)
     .pipe(todo())
     .pipe(markdownToHTML())
     .pipe(gulp.dest('./docs/'));
 });
 //Tarea 'jsDoc'. Crea una documentacion HTML de todas las funciones.
-gulp.task('jsDoc', ['minificarJS'], function() {
+gulp.task('jsDoc', function() {
   return gulp.src(jsToWatch)
     .pipe(concat('jsdoc.md'))
     .pipe(jsdoc())
@@ -51,10 +52,6 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('./css/'));
 });
 
-//Vigilante, al hacerlo como una tarea, no ejecuta nada por defecto al abrir.
-gulp.task('watch', function() {
-  gulp.watch(jsToWatch, ['minificarJS', 'jsDoc', 'todo']).on('end', function(){
-    console.log('-------Compilation successful-------');
-  });
-  gulp.watch('./css/scss/**/*.scss', ['styles']);
-});
+/*********************WATCHERS*********************/
+gulp.task('watch', require('./gulp/watchers'));
+/*******************END WATCHERS*******************/
